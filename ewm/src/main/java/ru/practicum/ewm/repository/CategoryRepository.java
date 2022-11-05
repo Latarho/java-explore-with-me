4fcf4bda3106142c737bd4fa9f5aa9fba0592ff3
latarho <latarho@gmail.com>
2022-11-05 23:39:35 +0300

    Рефакторинг - Исправления в соответствии с комментариями ревьювера

--- ewm/src/main/java/ru/practicum/ewm/repository/CategoryRepository.java
+++ ewm/src/main/java/ru/practicum/ewm/repository/CategoryRepository.java
@@ -1,7 +1,7 @@
 package ru.practicum.ewm.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.model.category.Category;
-import ru.practicum.ewm.utils.jpa.CustomJpaRepository;
 
-public interface CategoryRepository extends CustomJpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
